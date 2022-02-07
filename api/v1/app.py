db4071ea6c180024fec7b44080dbb05e31b49eba
Kashuhappy <knashipa@gmail.com>
2022-02-07 07:16:57 -0800

    API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
-"""connect to API"""
-import os
+""" Status of our API """
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
+from os import getenv
+from flask import Flask, make_response, jsonify
+from flask import Blueprint
 from flask_cors import CORS
 
+"""Host and port env variables"""
+host_env = getenv('HBNB_API_HOST') or '0.0.0.0'
+port_env = getenv('HBNB_API_PORT') or 5000
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(code):
+def teardown_db(error):
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+    app.run(
+        host=host_env, port=port_env,
+        debug=True, threaded=True,
+    )
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,34 +1,25 @@
 #!/usr/bin/python3
-"""index.py to connect to API"""
+""" index file foe view"""
+from flask import jsonify
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
 from models import storage
 
 
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
-
-
-@app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
+@app_views.route('/status')
+def status():
+    """ Return status of the request """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
-
-
-if __name__ == "__main__":
-    pass
+@app_views.route('/stats')
+def stats():
+    """
+    endpoint retrieves the number of each object by type
+    """
+    objects = {"amenities": storage.count("Amenity"),
+               "cities": storage.count("City"),
+               "places": storage.count("Place"),
+               "reviews": storage.count("Review"),
+               "states": storage.count("State"),
+               "users": storage.count("User")}
+    return jsonify(objects)
